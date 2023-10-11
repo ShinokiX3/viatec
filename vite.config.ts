9b98c283cfa3c36dbb4693feb5f93d5b219cafab
shinoki <endernasha85@gmail.com>
2023-10-11 18:40:27 +0300

    environment setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+			'@assets': resolve(__dirname, './src/assets'),
+			'@components': resolve(__dirname, './src/components/'),
+			'@types': resolve(__dirname, './src/types/'),
+			'@pages': resolve(__dirname, './src/pages/'),
+			'@store': resolve(__dirname, './src/store/'),
+			'@hooks': resolve(__dirname, './src/hooks/'),
+			'@utils': resolve(__dirname, './src/utils/'),
+		},
+	},
+});
